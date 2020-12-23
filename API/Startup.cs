a829f8aa4432eafc7c92611905f9e5336825b2e6
srii28 <srii.28@gmail.com>
2020-12-22 18:03:40 -0600

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,6 +18,7 @@ namespace API
 {
     public class Startup
     {
+        
         private readonly IConfiguration _config;
         public Startup(IConfiguration config)
         {
@@ -35,6 +36,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -54,6 +56,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(X=>X.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
